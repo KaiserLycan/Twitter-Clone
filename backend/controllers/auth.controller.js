dd0534ddaaef9f79894371717a02a93563a1404d
Joseph Rey R. Panti <josephreyramirezpanti+github@gmail.com>
2025-12-29 12:05:19 +0800

    -1 Auth Controller Functions

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,12 +1,113 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import {generateTokenAndSetCookie} from "../lib/utils/generateToken.js";
 
 export const signup = async (req, res) => {
-    res.send("signup request");
+    try{
+        const {fullName, username, email, password} = req.body;
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)) {
+            return res.status(400).send({ error: "Invalid email format"});
+        }
+
+        const existingUser = await User.findOne({username});
+        if(existingUser) {
+            return res.status(400).send({ error: " Username is already in use" });
+        }
+
+        const existingEmail = await User.findOne({email});
+        console.log(existingEmail)
+        if(existingEmail) {
+            return res.status(400).send({ error: " Email is already in use" });
+        }
+
+
+        if(password.length < 8) {
+            return res.status(400).send({ error: "Password must be at least 8 characters" });
+        }
+        // hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            username,
+            password: hashedPassword,
+        })
+
+        if(newUser) {
+            generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save();
+
+            return res.status(201).json( {
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                email: newUser.email,
+                followers: newUser.followers,
+                following: newUser.following,
+                profileImg: newUser.profileImg,
+                coverImg: newUser.coverImg
+            })
+        }
+        else {
+            return res.status(400).json({ error: "Invalid user data" });
+        }
+
+    }
+    catch (error) {
+        console.log("Error in signup controller ", error.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
 }
 
 export const login = async (req, res) => {
-    res.send("login request");
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+        if(!user || !isPasswordCorrect) {
+            return res.status(400).send({ error: "Invalid username or password" });
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+        return res.status(200).json( {
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            email: user.email,
+            followers: user.followers,
+            following: user.following,
+            profileImg: user.profileImg,
+            coverImg: user.coverImg
+        });
+    }
+    catch (error) {
+        console.log("Error in login controller ", error.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
 }
 
 export const logout = async (req, res) => {
-    res.send("logout request");
+    try {
+        res.cookie("jwt", "", {maxAge: 0});
+        res.status(200).json({message: "Logged out successfully"});
+    }
+    catch (error) {
+        console.log("Error in logout controller ", error.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+export const getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id).select("-password");
+        return res.status(200).json(user);
+    }
+    catch (error) {
+        console.log("Error in getMe controller ", error.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
 }
--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,21 @@ import dotenv from 'dotenv';
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json()); // to parse json data
+app.use(express.urlencoded({ extended: true })); // to parse form data
+app.use(cookieParser()); // to parse cookie
 
 console.log(process.env.MONGO_URI)
 
 //Routes
 app.use("/api/auth", authRoutes);
-
-app.get("/", (req, res) => {
-    res.send("Server is running");
-})
 
 app.listen(PORT, async () => {
     console.log(`Server running on http://localhost:${PORT}`);
