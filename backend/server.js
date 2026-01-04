3a8cf82aa6ab707c337033ffbf8cf797c50ca20d
Joseph Rey R. Panti <josephreyramirezpanti+github@gmail.com>
2026-01-04 09:14:01 +0800

    4- Prepared our app for deployment

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
+import path from "path";
 
 dotenv.config();
 
@@ -21,18 +22,25 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({limit: "5mb"})); // to parse json data
 app.use(express.urlencoded({ extended: true })); // to parse form data
 app.use(cookieParser()); // to parse cookie
-
-console.log(process.env.MONGO_URI)
 
 //Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, async () => {
     console.log(`Server running on http://localhost:${PORT}`);
